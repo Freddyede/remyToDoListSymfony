ea65b8d978c6a326a8211d8049d3f8a1a1fc2c29
Freddyede <patouillardfranck3@gmail.com>
2023-12-05 16:09:23 +0100

    0.0.3-Tasks
    - Add Authentification

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\{HttpFoundation\Response, Routing\Annotation\Route};
+use Symfony\Component\{
+    HttpFoundation\Response,
+    Routing\Annotation\Route,
+    Security\Http\Attribute\IsGranted
+};
 
-#[Route('/tasks', name: 'tasks.')]
+#[IsGranted("ROLE_ADMIN")]
+#[Route('/admin/tasks', name: 'tasks.')]
 class TasksController extends AbstractController
 {
     #[Route('/', name: 'index')]
